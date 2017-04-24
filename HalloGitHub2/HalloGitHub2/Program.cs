273df3f0800f6b96527c1deec0f640ed7b34e1c3
Mikio Osaki <s0554232@htw-berlin.de>
2017-04-24 22:22:37 +0200

    Dritte Bearbeitung (+Methode Add)

--- HalloGitHub2/HalloGitHub2/Program.cs
+++ HalloGitHub2/HalloGitHub2/Program.cs
@@ -11,7 +11,18 @@ namespace HalloGitHub2
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo GitHub2");
+            Console.WriteLine("enter a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter b");
+            double b = double.Parse(Console.ReadLine());
+            double erg = Add(a, b);
+            Console.WriteLine("{0}+{1}={2}",a,b,erg);
             Console.ReadLine();
+        }
+
+        static double Add(double a, double b)
+        {
+            return a+b;
         }
     }
 }
